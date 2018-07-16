784988a1e0b774ab1f27968891da92baa9501b9f
Shinya Kumagai <roomful.rooms@gmail.com>
2018-07-17 00:05:48 +0900

    WIP

--- src/module/model/result.ts
+++ src/module/model/result.ts
@@ -1,29 +1,79 @@
-export default class Result<T, E extends Error | undefined> {
-  public static EMPTY = new Result<undefined, undefined>(false, undefined, undefined);
+import * as Immutable from "immutable";
 
-  public static inProgress(): Result<undefined, undefined> {
-    return new Result<undefined, undefined>(true, undefined, undefined);
+// export default class Result<T, E extends Error | undefined> {
+//   public static EMPTY = new Result<undefined, undefined>(false, undefined, undefined);
+
+//   public static inProgress(): Result<undefined, undefined> {
+//     return new Result<undefined, undefined>(true, undefined, undefined);
+//   }
+
+//   public static success<T>(data: T): Result<T, undefined> {
+//     return new Result(false, data, undefined);
+//   }
+
+//   public static error<E extends Error>(e: E): Result<undefined, E> {
+//     return new Result(false, undefined, e);
+//   }
+
+//   private constructor(
+//     public readonly inProgress: boolean,
+//     public readonly data: T | undefined,
+//     public readonly error: E | undefined,
+//   ) {}
+
+//   public isSuccess(): boolean {
+//     return (
+//       this.inProgress !== false &&
+//       this.error === undefined &&
+//       this.data !== undefined
+//     );
+//   }
+// }
+
+export class Result<T, E extends Error = Error> extends Immutable.Record({
+  inProgress: false
+}) {
+  public static EMPTY = new Result<undefined>({ inProgress: false });
+
+  public readonly inProgress!: boolean;
+
+  public readonly data: T | undefined;
+
+  public readonly error: E | undefined;
+
+  constructor(values: Result.Props<T, E>) {
+    super(values);
   }
 
-  public static success<T>(data: T): Result<T, undefined> {
-    return new Result(false, data, undefined);
+  public asInProgress(): Result<undefined> {
+    return this.withMutations(s => {
+      s.set("inProgress", true)
+        .remove("data")
+        .remove("error");
+    }) as Result<undefined>;
   }
 
-  public static error<E extends Error>(e: E): Result<undefined, E> {
-    return new Result(false, undefined, e);
+  public asSuccess(data: T): Result<T> {
+    return this.withMutations(s => {
+      s.set("inProgress", false)
+        .set("data", data)
+        .remove("error");
+    }) as Result<T>;
   }
 
-  private constructor(
-    public readonly inProgress: boolean,
-    public readonly data: T | undefined,
-    public readonly error: E | undefined,
-  ) {}
-
-  public isSuccess(): boolean {
-    return (
-      this.inProgress !== false &&
-      this.error === undefined &&
-      this.data !== undefined
-    );
+  public asError(e: E): Result<undefined, E> {
+    return this.withMutations(s => {
+      s.set("inProgress", false)
+        .remove("data")
+        .set("error", e);
+    }) as Result<undefined, E>;
   }
+}
+
+namespace Result {
+  export type Props<T, E extends Error = Error> = {
+    inProgress: boolean;
+    data?: T;
+    error?: E;
+  };
 }
--- src/module/state/task/reducer.ts
+++ src/module/state/task/reducer.ts
@@ -1,2 +1,7 @@
 import { Task, TaskList } from "../../model/task";
+import { FSA } from "flux-standard-action";
+
+export default function taskReducer(state: any, action: FSA<any>): any {
+
+}
 
