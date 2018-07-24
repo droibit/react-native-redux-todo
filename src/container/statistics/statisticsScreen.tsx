e61977cc6dceab63781d9f27e85686261cbf9fb8
Shinya Kumagai <roomful.rooms@gmail.com>
2018-07-24 19:12:22 +0900

    Implement StatisticsScreen

--- src/container/statistics/statisticsScreen.tsx
+++ src/container/statistics/statisticsScreen.tsx
@@ -1,14 +1,35 @@
 import React, {
   Component
 } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import { NavigationScreenOptions } from "react-navigation";
-import { Dispatch, Action } from "redux";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import { Container, Content, Icon } from "native-base";
+import { TaskStateProps } from "../../module/state/type";
+import { countCompletedTask, countActiveTask } from "../../module/state/task/selector";
 
 type Props = {
+  activeTaskCount: number;
+  completedTaskCount: number;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  label: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginLeft: 8,
+  },
+});
+
 class StatisticsScreen extends Component<Props> {
   static navigationOptions: NavigationScreenOptions = {
     title: "Statistics",
@@ -19,31 +40,28 @@ class StatisticsScreen extends Component<Props> {
   }
 
   public render() {
+    const { activeTaskCount, completedTaskCount } = this.props;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Statistics!</Text>
-        <Icon name="done" size={30} color="#900" />
-      </View>
+      <Container>
+        <Content contentContainerStyle={styles.container}>
+          <View style={styles.item}>
+            <Icon name="schedule" type="MaterialIcons" />
+            <Text style={styles.label}>{"Active TO-DOs: " + activeTaskCount}</Text>
+          </View>
+          <View style={styles.item}>
+            <Icon name="done" type="MaterialIcons" />
+            <Text style={styles.label}>{"Completed TO-DOs: " + completedTaskCount}</Text>
+          </View>
+        </Content>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  }
-});
-
-const mapStateToProps = (state: any) => {
+const mapStateToProps = (state: TaskStateProps): Partial<Props> => {
   return {
+    activeTaskCount: countActiveTask(state),
+    completedTaskCount: countCompletedTask(state),
   };
 };
 
--- src/module/state/task/selector.ts
+++ src/module/state/task/selector.ts
@@ -5,6 +5,7 @@ import {
 import {
   Task,
   TaskList,
+  TaskState,
 } from "../../model/task";
 import {
   TaskSortSetting,
@@ -25,27 +26,41 @@ export const filteredAndSortedTasks = createSelector<TaskStateProps & AppSetting
     const destTasks = srcTasks.tasks.filter((t) => {
       switch (visibilityFilter) {
         case TaskVisibilityFilter.ALL:
-        return true;
+          return true;
         case TaskVisibilityFilter.ACTIVE:
-        return t!.isActive;
+          return t!.isActive;
         case TaskVisibilityFilter.COMPLETED:
-        return t!.completed;
+          return t!.completed;
       }
     }).sort((lhs, rhs) => {
       switch (sortSetting.taskSortBy) {
         case TaskSortBy.TITLE:
-        return (sortSetting.taskSortByOrder == TaskSortByOrder.ASC) ?
-        lhs.title.localeCompare(rhs.title) :
-        rhs.title.localeCompare(lhs.title);
+          return (sortSetting.taskSortByOrder == TaskSortByOrder.ASC) ?
+            lhs.title.localeCompare(rhs.title) :
+            rhs.title.localeCompare(lhs.title);
         case TaskSortBy.TIMESTAMP:
-        // ref. https://github.com/Microsoft/TypeScript/issues/5710
-        return (sortSetting.taskSortByOrder == TaskSortByOrder.ASC) ?
-        +lhs.timestamp - +rhs.timestamp :
-        +rhs.timestamp - +lhs.timestamp;
+          // ref. https://github.com/Microsoft/TypeScript/issues/5710
+          return (sortSetting.taskSortByOrder == TaskSortByOrder.ASC) ?
+            +lhs.timestamp - +rhs.timestamp :
+            +rhs.timestamp - +lhs.timestamp;
       }
     })
       .toArray();
     console.log(`filteredAndSortedTasks(srcTasks(len)=${srcTasks.length}, sortSetting=${JSON.stringify(sortSetting)}, visibilityFilter=${JSON.stringify(visibilityFilter)}): ${JSON.stringify(destTasks)}`);
     return destTasks;
   }
-)
+);
+
+export const countActiveTask = createSelector<TaskStateProps, TaskList, number>(
+  (state) => state.task.tasks,
+  (srcTasks) => {
+    return srcTasks.tasks.count(t => t!.isActive);
+  }
+);
+
+export const countCompletedTask = createSelector<TaskStateProps, TaskList, number>(
+  (state) => state.task.tasks,
+  (srcTasks) => {
+    return srcTasks.tasks.count(t => t!.completed);
+  }
+);
